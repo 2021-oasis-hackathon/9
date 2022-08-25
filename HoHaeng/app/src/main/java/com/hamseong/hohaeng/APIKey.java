33cf4ceafb000c10bc531206a20b2475e7f8e195
K_Gs:D <76468787+rootachieve@users.noreply.github.com>
2022-08-25 13:17:04 +0900

    Delete APIKey.java
    I accidentally uploaded apikey and changed apikey. This apikey is no longer in use.

--- HoHaeng/app/src/main/java/com/hamseong/hohaeng/APIKey.java
+++
@@ -1,7 +0,0 @@
-package com.hamseong.hohaeng;
-
-public class APIKey {
-    public static final String KaKaoApi = "KakaoAK 1a849348543d0e63139415ce151e56a8";
-    public static final String KaKaoUrl ="https://dapi.kakao.com/";
-    public static final String TmapApi = "l7xxda5721f9e50b48ae96db72c6d00fa7ef";
-}
